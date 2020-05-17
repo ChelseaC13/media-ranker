ced82a9decddf214b8baa4fb22302b44fce4e190
ChelseaC13 <chelhelg@outlook.com>
2020-05-17 12:58:43 -0700

    form made for add work but not submitting

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,4 +11,47 @@ class WorksController < ApplicationController
 
   end
 
+
+  def create
+    @work = Work.new(work_params) 
+    
+    if @work.save 
+      redirect_to works_path(@work.id)
+      return
+    else 
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path 
+      return
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to works_path 
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
