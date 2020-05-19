fcd2518688b5860fcebe9aa891a614a8a4c5e949
ChelseaC13 <chelhelg@outlook.com>
2020-05-19 16:56:55 -0700

    fixed migrations had to do a stash need to redo validation tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_204836) do
+ActiveRecord::Schema.define(version: 2020_05_19_234525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 2020_05_19_204836) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "work_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -37,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_05_19_204836) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
